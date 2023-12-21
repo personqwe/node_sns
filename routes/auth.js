891765f4122c289b111ecf96b85b5c772fae73c1
DESKTOP-8BT8B8U\kyoun <kyoungae1231@naver.com>
2023-12-21 19:29:42 +0900

    [Node] - 카카오 회원가입 구현

--- routes/auth.js
+++ routes/auth.js
@@ -11,4 +11,17 @@ router.post('/login', isNotLoggedIn, login);
 
 router.get('/logout', isLoggedIn, logout);
 
+// /auth/kakao
+router.get('/kakao', passport.authenticate('kakao')); // 카카오톡 로그인 화면으로 redirect
+// /auth/kakao -> 카카오톡 로그인 화면 -> 로그인이 성공 -> /auth/kakao/callback
+// /auth/kakao/callback
+router.get('/kakao/callback', passport.authenticate('kakao', { // 카카오 로그인이 되면 여기서 다시 redirect
+    failureRedirect: '/?LoginError=카카오로그인 실패',
+}), (req, res) => {
+    res.redirect('/');
+});
+
 module.exports = router;
+
+//app.use(passort.authenticate('kakao)); = 기본 기능만 사용
+//app.use((req, res, next) => passport.authenticate('kakkao')(req, res, next)) = req, res, next를 사용하고 싶을 때
